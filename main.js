b1b98f203011810d72c4d2abf0d1f76b2ef56e89
NurullohUbaydullayev <nurulloh.ubaydullayev2004@gmail.com>
2021-06-13 23:16:16 +0500

    Add all files

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const elHamburgerBtn = document.querySelector('.menu-btn')
 const elHeader = document.querySelector('.header')
 
+const elArrowBtn = document.querySelector('.how-works__item__btn')
+const elHowWorksItem = document.querySelector('.how-works__list__item')
+
 elHamburgerBtn.addEventListener('click', function(){
     elHeader.classList.toggle('header--adaptive')
+})
+
+elArrowBtn.addEventListener('click', function(){
+    elHowWorksItem.classList.toggle('how-works__list__item--active')
 })
